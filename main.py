bc1252ab7f2824ad0b0b98c745b9bc8c2c923456
Aryan Sonone <72177377+Python-aryan@users.noreply.github.com>
2021-10-06 20:05:45 +0530

    Addition of Code
    Some of the comments were unnecessary and some codes were missing.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import re
+ import re
 
 print("Welcome To Exerise 11\n"
       "Email Collector\n")
+# strin =
+
+
+
+# Email Collector
+# email1
+# email2
+# email3
 
 str = """
+
 Email:enquiry@alliance.edu.in   Helpline: +91 80 3093 8100 / 8200 / 4619 9100
 Media  Library  News  Webmail  Careers
 Alliance University
@@ -27,37 +36,46 @@ Contact UsHome Contact Us
 Dr. Pavana Dibbur
  : vc@alliance.edu.in
  : +91 80 3093 8100/4619 9100
+
  Pro Vice-Chancellor (Academics, Research & Strategy)
 Dr. Anubha Singh
  : anubha@alliance.edu.in
  : +91 80 3093 8102
+
  Registrar
 Mr. Madhu Sudan Mishra
  : registrar@alliance.edu.in
  : +91 80 3093 8100/4619 9100
+
  Registrar (Examination & Evaluation)
 Dr. Sajan Mathew
  : registrar.exams@alliance.edu.in
  : +91 80 3093 8141
+
  Director (Placements)
 Mr. Mathew Thankachan
  : placement@alliance.edu.in | mathew.t@alliance.edu.in
  : +91 80 3093 8124 | 98444 72674
+
  Director (International Affairs)
 Mr. Rajen Chatterjee
  : rajen.chatterjee@alliance.edu.in
  : +91 80 3093 8075
+
  Director (Admissions)
 Mr. Abhay Chebbi
  : abhay.chebbi@alliance.edu.in
  : +91 96636 46464
+
  Human Resources Department
  : hrd@alliance.edu.in
  : +91 80 3093 8210 / 8204
+
  Student Verification 
 Office of Registrar (Examination & Evaluation)
  : edu.verify@alliance.edu.in
  : +91 80 3093 8100 / 8200 | +91 80 4619 9100
+
  Contacts Info
  ALLIANCE UNIVERSITY
  Central Campus
@@ -65,23 +83,29 @@ Chikkahagade Cross, Chandapura - Anekal Main Road, Anekal
 Bengaluru – 562 106, Karnataka,   India. [ Get Route Map ]
 +91 80 3093 8100/8200/4619 9100 | Fax: +91 80 4619 9099
 E-mail : enquiry@alliance.edu.in
+
  Office of Admissions
 UG: +91 9620009825 | 91084 43123 | 91084 42143 | 98806 19618
 PG: +91 98860 02500 | 99002 29974 | 90083 16363
+
  City Campus 1
 19th Cross, 7th Main, BTM 2nd Stage, N.S. Palya
 Bengaluru – 560 076, Karnataka,   India. [ Get Route Map ]
 Tel.: +91 80 26786020 / 21 , 26789749
+
  City Campus 2
 2nd Cross, 36th Main, Dollars Scheme, BTM 1st Stage
 Bengaluru – 560 068, Karnataka,   India. [ Get Route Map ]
 Tel.: +91 80 26681444 / 4372 | Fax: +91 80 26782048
+
  CONTACT INFO
  Contact Us
  Enquiry
  Feedback
  Get Route from Address
 Quick Course Finder
+
+
 Find Courses
  SCHOOLS | COLLEGES
  Alliance School of Business
@@ -90,6 +114,7 @@ Find Courses
  Alliance Ascent College
  Planned Academic Units
 International Partners
+
 Antwerp Management School
 Antwerp Management School
 Belgium
@@ -189,8 +214,13 @@ USA
  International Programs
  Testimonials
  My two years at Alliance University have groomed me to be a confident individual ready to enter the corporate world and has deepened this confidence by helping me get a job in my dream organization. Alliance with its state of the art facilities, competitive curriculum, varied cultural mix and strong faculty base has motivated and guided m...  Read More
+
  Kiran Varghese Jacob Kiran Varghese Jacob
 Google Google
  Top University in India for MBA LAW Engineering & Arts and the Humanities
 Alliance University is a private University established in Karnataka State by Act No.34 of year 2010 and is recognized by the University Grants Commission (UGC), New Delhi... 
+
 """
+
+email = re.findall(r"[a-zA-Z0-9_.-]+@[a-zA-Z0-9_.-]+\.[a-zA-Z]+",str)
+print(f"The emails we found are :\n{email}")
